d7efe6c7fd20a6d4c843b184541fc7c19cd7f70a
saurabhmithe <saurabhmithe@gmail.com>
2018-12-18 00:04:07 -0800

    Initial Commit

---
+++ Tipster - The Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipster - The Tip Calculator
+//
+//  Created by Saurabh Mithe on 12/18/18.
+//  Copyright © 2018 Saurabh Mithe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
